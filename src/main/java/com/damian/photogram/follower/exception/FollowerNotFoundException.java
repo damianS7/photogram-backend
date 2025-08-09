4955c003575ca14c9eb4262e99f1d4778c992767
Damian <josepwnz@gmail.com>
2025-08-09 21:11:45 +0200

    removed old clases since renamed to Follow

--- src/main/java/com/damian/photogram/follower/exception/FollowerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.damian.photogram.follower.exception;
-
-import com.damian.photogram.common.exception.ApplicationException;
-
-public class FollowerNotFoundException extends ApplicationException {
-    public FollowerNotFoundException(String message) {
-        super(message);
-    }
-}
