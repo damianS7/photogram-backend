b7848595689c6655c9764548da3feb55a9ed10bb
Damian <josepwnz@gmail.com>
2025-08-17 23:37:37 +0200

    deleted

--- src/main/java/com/damian/photogram/domain/post/exception/PostAuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.damian.photogram.domain.post.exception;
-
-import com.damian.photogram.app.auth.exception.AuthorizationException;
-
-public class PostAuthorizationException extends AuthorizationException {
-    public PostAuthorizationException(String message) {
-        super(message);
-    }
-}
