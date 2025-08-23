da3e23d4e25440cfc30fac0d3baceca046121a1b
Damian <josepwnz@gmail.com>
2025-08-23 19:10:03 +0200

    switched to BadCredentialsException

--- src/main/java/com/damian/photogram/core/service/CustomerDetailsService.java
+++ src/main/java/com/damian/photogram/core/service/CustomerDetailsService.java
@@ -1,9 +1,9 @@
 package com.damian.photogram.core.service;
 
-import com.damian.photogram.core.security.CustomerDetails;
 import com.damian.photogram.core.exception.Exceptions;
-import com.damian.photogram.app.auth.exception.AuthenticationBadCredentialsException;
+import com.damian.photogram.core.security.CustomerDetails;
 import com.damian.photogram.domain.customer.repository.CustomerRepository;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,7 +26,7 @@ public class CustomerDetailsService implements UserDetailsService {
         return customerRepository
                 .findByEmail(email)
                 .orElseThrow(
-                        () -> new AuthenticationBadCredentialsException(
+                        () -> new BadCredentialsException(
                                 Exceptions.ACCOUNT.BAD_CREDENTIALS
                         )
                 );
