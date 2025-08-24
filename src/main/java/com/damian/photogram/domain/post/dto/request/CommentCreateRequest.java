c38b083a8f46a5e00d823d5a9be8d0267e4ba7d3
Damian <josepwnz@gmail.com>
2025-08-24 02:11:07 +0200

    added validation

--- src/main/java/com/damian/photogram/domain/post/dto/request/CommentCreateRequest.java
+++ src/main/java/com/damian/photogram/domain/post/dto/request/CommentCreateRequest.java
@@ -1,6 +1,9 @@
 package com.damian.photogram.domain.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CommentCreateRequest(
+        @NotBlank(message = "Comment cannot be empty")
         String comment
 ) {
 }
