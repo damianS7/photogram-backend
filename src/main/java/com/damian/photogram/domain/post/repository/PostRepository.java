e35c8e3b136bf6472c87a00d3c8254865fb9be12
Damian <josepwnz@gmail.com>
2025-08-24 02:10:54 +0200

    removed unused query

--- src/main/java/com/damian/photogram/domain/post/repository/PostRepository.java
+++ src/main/java/com/damian/photogram/domain/post/repository/PostRepository.java
@@ -7,12 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Set<Post> findAllByAuthorId(Long authorId);
-
     @Query("SELECT p FROM Post p WHERE p.author.profile.username = :username")
     Page<Post> findAllByUsername(String username, Pageable pageable);
 
