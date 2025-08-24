2202dad0897e83eee06e188eb6f69f8356a251de
Damian <josepwnz@gmail.com>
2025-08-24 21:57:25 +0200

    removed unused method

--- src/main/java/com/damian/photogram/domain/customer/helper/ProfileHelper.java
+++ src/main/java/com/damian/photogram/domain/customer/helper/ProfileHelper.java
@@ -1,15 +1,7 @@
 package com.damian.photogram.domain.customer.helper;
 
-import com.damian.photogram.domain.customer.model.Customer;
-import com.damian.photogram.domain.customer.model.Profile;
-
 public class ProfileHelper {
     private static final String PROFILE_IMAGE_UPLOAD_PATH = "customers/{id}/";
-
-    public static boolean isAuthor(Customer customer, Profile profile) {
-        // check if the customer is the author of the profile.
-        return customer.getId().equals(profile.getCustomerId());
-    }
 
     public static String getProfileImageUploadPath(Long customerId) {
         return PROFILE_IMAGE_UPLOAD_PATH.replace("{id}", customerId.toString());
