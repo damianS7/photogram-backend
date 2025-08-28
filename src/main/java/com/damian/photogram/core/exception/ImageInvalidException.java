6c5ce5062c643d026386086826607cdc2e5f1a04
Damian <josepwnz@gmail.com>
2025-08-28 23:47:41 +0200

    removed unused exception

--- src/main/java/com/damian/photogram/core/exception/ImageInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.damian.photogram.core.exception;
-
-public class ImageInvalidException extends ApplicationException {
-    public ImageInvalidException(String message) {
-        super(message);
-    }
-}
