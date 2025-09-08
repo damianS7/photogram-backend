74d2574c6c9ba4a6931d26eaa15866b7714888cb
Damian <josepwnz@gmail.com>
2025-09-09 00:59:20 +0200

    code cleanup

--- src/main/java/com/damian/photogram/app/notification/NotificationService.java
+++ src/main/java/com/damian/photogram/app/notification/NotificationService.java
@@ -1,8 +1,15 @@
 package com.damian.photogram.app.notification;
 
 import com.damian.photogram.app.notification.dto.NotificationEvent;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.damian.photogram.core.exception.Exceptions;
+import com.damian.photogram.core.utils.AuthHelper;
+import com.damian.photogram.domain.customer.exception.CustomerNotFoundException;
+import com.damian.photogram.domain.customer.model.Customer;
+import com.damian.photogram.domain.customer.repository.CustomerRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
 
@@ -11,43 +18,88 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class NotificationService {
-    private final RabbitTemplate rabbitTemplate;
-    private final Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+    private final NotificationRepository notificationRepository;
+    private final CustomerRepository customerRepository;
     private final Map<Long, Sinks.Many<NotificationEvent>> userSinks = new ConcurrentHashMap<>();
 
-    public NotificationService(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
+    public NotificationService(
+            NotificationRepository notificationRepository,
+            CustomerRepository customerRepository
+    ) {
+        this.notificationRepository = notificationRepository;
+        this.customerRepository = customerRepository;
     }
 
-    public void publish(String message) {
-        sink.tryEmitNext(message);
+    /**
+     * Get notifications for the current customer.
+     *
+     * @param pageable pagination params
+     * @return Page<Notification> a page of notifications
+     */
+    public Page<Notification> getNotifications(Pageable pageable) {
+        Customer customer = AuthHelper.getLoggedCustomer();
+        return notificationRepository.findAllByCustomerId(customer.getId(), pageable);
     }
 
-    //    public void publish(Object notification) {
-    //        rabbitTemplate.convertAndSend(
-    //                "appExchange",
-    //                "notifications.key",
-    //                notification
-    //        );
-    //    }
+    /**
+     * Delete all notifications for the current customer.
+     */
+    @Transactional
+    public void deleteNotifications() {
+        Customer currentCustomer = AuthHelper.getLoggedCustomer();
 
-    public Flux<String> getStream() {
-        return sink.asFlux();
+        // delete all notifications
+        notificationRepository.deleteAllByCustomer_Id(currentCustomer.getId());
     }
 
-    public Flux<NotificationEvent> getNotificationsForUser(Long userId) {
-        return userSinks
-                .computeIfAbsent(userId, k -> Sinks.many().multicast().onBackpressureBuffer())
-                .asFlux();
+    /**
+     * Get notifications for the current customer as a Flux stream.
+     * The stream will be closed when the client disconnects.
+     *
+     * @return Flux<NotificationEvent> a stream of notifications
+     */
+    public Flux<NotificationEvent> getNotificationsForUser() {
+        Customer customer = AuthHelper.getLoggedCustomer();
+
+        // create a sink for the user if not exists
+        Sinks.Many<NotificationEvent> sink = userSinks.computeIfAbsent(
+                customer.getId(),
+                k -> Sinks.many().multicast().onBackpressureBuffer()
+        );
+
+        // remove when disconnect
+        return sink.asFlux().doOnCancel(() -> {
+            userSinks.remove(customer.getId());
+        });
     }
 
-    public void publish(NotificationEvent event, Long recipientId) {
+    /**
+     * Publish a notification event to the recipient.
+     *
+     * @param event the notification event
+     */
+    public void publish(NotificationEvent event) {
+        Customer customer = AuthHelper.getLoggedCustomer();
         // if the receiverId is the same as senderId then do nothing
-        if (event.senderId().equals(recipientId)) {
+        if (customer.getId().equals(event.recipientId())) {
             return;
         }
 
-        var sink = userSinks.get(recipientId);
+        // find recipient customer who will receive the notification
+        Customer recipient = customerRepository.findById(event.recipientId()).orElseThrow(
+                () -> new CustomerNotFoundException(Exceptions.CUSTOMER.NOT_FOUND)
+        );
+
+        // create and save notification
+        Notification notification = Notification
+                .create(recipient)
+                .setMessage(event.message())
+                .setMetadata(event.metadata())
+                .setType(event.type());
+        notificationRepository.save(notification);
+
+        // emit event to the recipient if connected
+        var sink = userSinks.get(event.recipientId());
         if (sink != null) {
             sink.tryEmitNext(event);
         }
