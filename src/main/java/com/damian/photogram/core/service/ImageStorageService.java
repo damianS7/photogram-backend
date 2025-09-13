3ea73525db93468132d54aff6b1577736becd3f3
Damian <josepwnz@gmail.com>
2025-09-13 04:37:39 +0200

    Moved storeFile from ImageUploaderService to ImageStorageService

--- src/main/java/com/damian/photogram/core/service/ImageStorageService.java
+++ src/main/java/com/damian/photogram/core/service/ImageStorageService.java
@@ -1,24 +1,26 @@
 package com.damian.photogram.core.service;
 
 import com.damian.photogram.core.exception.Exceptions;
+import com.damian.photogram.core.exception.ImageFailedUploadException;
 import com.damian.photogram.core.exception.ImageNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 /**
  * Service class for handling image storage and retrieval.
  */
 @Service
 public class ImageStorageService {
-    private final String UPLOAD_IMAGE_PATH = "uploads/images/";
+
+    private static final Logger log = LoggerFactory.getLogger(ImageStorageService.class);
 
     /**
      * Creates a Resource from the given path.
@@ -38,6 +40,25 @@ public class ImageStorageService {
         return resource;
     }
 
+    // stores the image
+    public void storeImage(MultipartFile file, String path, String filename) {
+        if (!path.startsWith(ImageUploaderService.ROOT_UPLOAD_FOLDER)) {
+            throw new ImageFailedUploadException(Exceptions.IMAGE.UPLOAD_FAILED);
+        }
+
+        try {
+            Path uploadPath = Paths.get(path);
+            Files.createDirectories(uploadPath);
+            Path filePath = uploadPath.resolve(filename);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            // TODO chck the exception type
+            throw new ImageFailedUploadException(
+                    Exceptions.IMAGE.UPLOAD_FAILED
+            );
+        }
+    }
+
     /**
      * Returns a resource for the given folder and filename.
      *
@@ -46,9 +67,15 @@ public class ImageStorageService {
      * @return Resource object representing the image
      */
     public Resource getImage(String folderPath, String filename) {
+        log.info("Fetching image from folder: {}, filename: {}", folderPath, filename);
+
+        if (!folderPath.startsWith(ImageUploaderService.ROOT_UPLOAD_FOLDER)) {
+            throw new ImageNotFoundException(Exceptions.IMAGE.INVALID_PATH);
+        }
+
         Path filePath;
         try {
-            filePath = Paths.get(UPLOAD_IMAGE_PATH + folderPath).resolve(filename).normalize();
+            filePath = Paths.get(folderPath).resolve(filename).normalize();
         } catch (InvalidPathException exception) {
             throw new ImageNotFoundException(Exceptions.IMAGE.INVALID_PATH);
         }
@@ -62,7 +89,6 @@ public class ImageStorageService {
         return resource;
     }
 
-
     /**
      * Delete an image from server storage
      *
@@ -71,7 +97,7 @@ public class ImageStorageService {
      */
     public void deleteImage(String folder, String filename) {
         try {
-            Path pathToFile = Path.of(UPLOAD_IMAGE_PATH + folder + "/" + filename);
+            Path pathToFile = Path.of(folder + "/" + filename);
             Files.deleteIfExists(pathToFile);
         } catch (IOException e) {
             throw new ImageNotFoundException(Exceptions.IMAGE.NOT_FOUND);
--- src/main/java/com/damian/photogram/core/service/ImageUploaderService.java
+++ src/main/java/com/damian/photogram/core/service/ImageUploaderService.java
@@ -1,16 +1,11 @@
 package com.damian.photogram.core.service;
 
-import com.damian.photogram.core.exception.*;
+import com.damian.photogram.core.utils.AuthHelper;
+import com.damian.photogram.domain.customer.model.Customer;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -18,64 +13,34 @@ import java.util.UUID;
  */
 @Service
 public class ImageUploaderService {
-    private final String IMAGE_PATH = "uploads/images/";
-    private final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
-    private final String[] ALLOWED_IMAGE_TYPE = {"image/jpg", "image/jpeg", "image/png"};
+    // root folder for uploads
+    public static final String ROOT_UPLOAD_FOLDER = "uploads/images/customers/";
+    private final ImageStorageService imageStorageService;
 
     public ImageUploaderService(
+            ImageStorageService imageStorageService
     ) {
+        this.imageStorageService = imageStorageService;
     }
 
-    // validations for file uploaded photos
-    public void validateImageOrElseThrow(MultipartFile file) {
-        if (file.isEmpty()) {
-            throw new ImageEmptyFileException(Exceptions.IMAGE.EMPTY_FILE);
-        }
-
-        String contentType = file.getContentType();
-        boolean imageTypeAllowed = Arrays
-                .stream(ALLOWED_IMAGE_TYPE)
-                .anyMatch(ct -> ct.equalsIgnoreCase(contentType));
-
-        if (!imageTypeAllowed) {
-            throw new ImageTypeNotAllowedException(Exceptions.IMAGE.TYPE_NOT_SUPPORTED);
-        }
-
-        if (file.getSize() > MAX_FILE_SIZE) {
-            throw new ImageFileSizeExceededException(Exceptions.IMAGE.TOO_LARGE);
-        }
-    }
-
-    // stores the image
-    private void storeFile(MultipartFile file, String folder, String filename) {
-        try {
-            Path uploadPath = Paths.get(IMAGE_PATH + folder);
-            Files.createDirectories(uploadPath);
-            Path filePath = uploadPath.resolve(filename);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new ImageFailedUploadException(
-                    Exceptions.IMAGE.UPLOAD_FAILED
-            );
-        }
+    public static String getCustomerUploadFolder(Long customerId) {
+        return ROOT_UPLOAD_FOLDER + customerId + "/";
     }
 
     /**
      * Uploads an image to the server
      */
     public String uploadImage(MultipartFile file, String folder, String filename) {
-        // run file validations
-        this.validateImageOrElseThrow(file);
-        // TODO compress or convert to webp image before uploading to server
+        final Customer currentCustomer = AuthHelper.getLoggedCustomer();
+        String path = getCustomerUploadFolder(currentCustomer.getId()) + folder;
 
         final String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
         if (extension != null && !filename.endsWith(extension)) {
             filename += "." + extension;
         }
 
-
         // saving file
-        this.storeFile(file, folder, filename);
+        imageStorageService.storeImage(file, path, filename);
 
         return filename;
     }
