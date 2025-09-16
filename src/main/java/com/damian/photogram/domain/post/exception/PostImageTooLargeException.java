050bb346e526743134945beaa4177bb6fe7c06e6
Damian <josepwnz@gmail.com>
2025-09-16 19:40:49 +0200

    deleted unused exception

--- src/main/java/com/damian/photogram/domain/post/exception/PostImageTooLargeException.java
+++
@@ -1,9 +0,0 @@
-package com.damian.photogram.domain.post.exception;
-
-import com.damian.photogram.core.exception.ApplicationException;
-
-public class PostImageTooLargeException extends ApplicationException {
-    public PostImageTooLargeException(String message) {
-        super(message);
-    }
-}
